ced81d3438c6a075913009e4bfad5ac49830583d
Paulo Miguel Almeida <paulo.ubuntu@gmail.com>
2015-02-09 13:46:27 -0200

    Changed js function names

--- www/tesseract_camera.js
+++ www/tesseract_camera.js
@@ -1,14 +1,14 @@
 var exec = cordova.require('cordova/exec');
 
-exports.image.recognize = function (imageData, charWhiteList, callbackSuccess, callbackFailure) {
+exports.recognize = function (imageData, charWhiteList, callbackSuccess, callbackFailure) {
     exec(callbackSuccess, callbackFailure, 'CordovaTesseractImageRecognizer', 'recognizeImage', [imageData, charWhiteList]);
 };
 
-exports.realtime.recognize.start = function(charWhiteList, callbackSuccess, callbackFailure){
+exports.realtimeRecognizerStart = function(charWhiteList, callbackSuccess, callbackFailure){
     exec(callbackSuccess, callbackFailure, 'CordovaTesseractRealTimeRecognizer', 'start', [charWhiteList]);
 }
 
-exports.realtime.recognize.close = function(callbackSuccess, callbackFailure){
+exports.realtimeRecognizeClose = function(callbackSuccess, callbackFailure){
     exec(callbackSuccess, callbackFailure, 'CordovaTesseractRealTimeRecognizer', 'stop', []);
 };
 
@@ -16,6 +16,6 @@ exports.takePicture = function (callbackSuccess, callbackFailure){
     exec(callbackSuccess, callbackFailure, 'CordovaTesseractCamera', 'takePicture', []);
 };
 
-exports.image.cropImage = function (imageData,callbackSuccess, callbackFailure){
+exports.cropImage = function (imageData,callbackSuccess, callbackFailure){
     exec(callbackSuccess, callbackFailure, 'CordovaTesseractImageCrop', 'cropImage', [imageData]);
 };
