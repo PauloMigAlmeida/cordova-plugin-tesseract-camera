e444b3ec4fbccc69898839b78fad4381d6bc5f26
Paulo Miguel Almeida <paulo.ubuntu@gmail.com>
2015-02-12 19:18:04 -0200

    Removed not needed code and added credits

--- src/ios/LDCTesseractCameraRectDetectionViewController.m
+++ src/ios/LDCTesseractCameraRectDetectionViewController.m
@@ -1,9 +1,34 @@
 //
 //  LDCTesseractCameraRectDetectionViewController.m
-//  InstaGraacc
 //
-//  Created by Paulo Miguel Almeida on 2/12/15.
+//  Version 0.0.1
 //
+//  The MIT License (MIT)
+//
+//  Original work Copyright (c) 2015 Maximilian Mackh <mmackh@gmail.com>
+//  Modified work Copyright (c) 2015 Paulo Miguel Almeida Rodenas <paulo.ubuntu@gmail.com>
+//
+//  Get the latest version from here:
+//
+//  https://github.com/mmackh/IPDFCameraViewController
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 #import "LDCTesseractCameraRectDetectionViewController.h"
@@ -27,7 +52,7 @@
     BOOL _isStopped;
     
     CGFloat _imageDedectionConfidence;
-//    NSTimer *_borderDetectTimeKeeper;
+    NSTimer *_borderDetectTimeKeeper;
     BOOL _borderDetectFrame;
     CIRectangleFeature *_borderDetectLastRectangleFeature;
     
@@ -215,10 +240,10 @@
     }
 }
 
-//- (void)enableBorderDetectFrame
-//{
-//    _borderDetectFrame = YES;
-//}
+- (void)enableBorderDetectFrame
+{
+    _borderDetectFrame = YES;
+}
 
 - (CIImage *)drawHighlightOverlayForPoints:(CIImage *)image topLeft:(CGPoint)topLeft topRight:(CGPoint)topRight bottomLeft:(CGPoint)bottomLeft bottomRight:(CGPoint)bottomRight
 {
@@ -235,7 +260,7 @@
     
     [self.captureSession startRunning];
     
-//    _borderDetectTimeKeeper = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(enableBorderDetectFrame) userInfo:nil repeats:YES];
+    _borderDetectTimeKeeper = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(enableBorderDetectFrame) userInfo:nil repeats:YES];
     
     [self hideGLKView:NO completion:nil];
 }
@@ -246,7 +271,7 @@
     
     [self.captureSession stopRunning];
     
-//    [_borderDetectTimeKeeper invalidate];
+    [_borderDetectTimeKeeper invalidate];
     
     [self hideGLKView:YES completion:nil];
 }
